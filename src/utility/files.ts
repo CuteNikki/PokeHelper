f97412bde616338a911435db6da1f88907a16c3b
Nikki <bravonikki13@gmail.com>
2026-04-08 09:42:27 +0200

    docs: add JSDoc comments for getFilesFrom function

--- src/utility/files.ts
+++ src/utility/files.ts
@@ -1,3 +1,8 @@
 import { globby } from 'globby';
 
+/**
+ * Get all .ts and .js files from a given relative path.
+ * @param relativePath - The relative path to search for files.
+ * @returns A promise that resolves to an array of absolute file paths.
+ */
 export const getFilesFrom = (relativePath: string) => globby([`${relativePath}/**/*{.ts,.js}`], { absolute: true });
